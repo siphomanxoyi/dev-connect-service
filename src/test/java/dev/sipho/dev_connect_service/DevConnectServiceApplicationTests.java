c19754b0c335285d01d0905d89de9dcff0e67f40
Sipho Manxoyi <siphosovuyomanxoyi@gmail.com>
2025-08-16 00:40:21 +0200

    initial commit

---
+++ src/test/java/dev/sipho/dev_connect_service/DevConnectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sipho.dev_connect_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevConnectServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
